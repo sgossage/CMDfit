fb7348118630bc23a38622ac04094a19fca20201
sgossage <sethg45@gmail.com>
2016-05-05 16:50:46 -0400

    Updated tests to refer to the new data file that I have swapped to; also updated to point to new model file as well.

--- cmdfit/tests/test_fit.py
+++ cmdfit/tests/test_fit.py
@@ -11,11 +11,11 @@ class TestIO(TestCase):
         root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         data_dir = os.path.join(root_dir, 'data')
         specific_data_dir = os.path.join(data_dir, 'Hyades')
-        data_file = os.path.join(specific_data_dir, 'goldman_less_Hyades.txt')
+        data_file = os.path.join(specific_data_dir, 'goldman_Hyades2MASS.txt')
         
         test_data = cread.get_header(data_file)
         
-        self.assertEqual(test_data[3], 'pmRA')
+        self.assertEqual(test_data[3], 'Vperp')
 
 # Tests that magnitude corrections will be applied correctly (this tests the AB to Vega correction and applies a distance modulus of 3.33 corresponding
 # to roughly what is believed for this cluster):
@@ -23,7 +23,7 @@ class TestABCorrections(TestCase):
     def test_corr(self):
         root_dir = data.select_pathtofile('test')
         ABtoVegafile = os.path.join(root_dir, 'data/convert_AB_to_Vega.txt')
-        model_file = os.path.join(root_dir, 'model/MIST_v0.31/HBlim005/MIST_v0.31_feh_m0.15_afe_p0.0_vvcrit0.4_03to8M_HBlim005_full.iso.cmd')
+        model_file = os.path.join(root_dir, 'model/MIST_v0.31/HBlim005/MIST_v0.31_feh_p0.15_afe_p0.0_vvcrit0.4_03to8M_HBlim005_full.iso.cmd')
         # Read in 2MASS J magnitudes from this model file:
         index2MASSJ = 12
         modelvalues = np.array(cread.get_values(model_file, index2MASSJ, mode='model'))
